725a48572feeec185fed24ea71ae04ad933cdcd8
JenniferDominique <j.dominiqueds@gmail.com>
2022-08-28 21:31:38 -0300

    feat(cronometro): Implementando o inicio da contagem do tempo da atividade

--- tracker/src/components/Formulario.vue
+++ tracker/src/components/Formulario.vue
@@ -9,15 +9,17 @@
             <div class="column">
                     <div class="is-flex is-align-items-center is-justify-content-space-between">
                         <section>
-                            <strong>00:00:00</strong>
+                            <strong>
+                                {{ timePassad }}
+                            </strong>
                         </section>
-                        <button class="button">
+                        <button class="button" @click="startTimer">
                             <span class="icon">
                                 <i class="fas fa-play" />
                             </span>
                             <span>Play</span>
                         </button>
-                        <button class="button">
+                        <button class="button" @click="stopTimer">
                             <span class="icon">
                                 <i class="fas fa-stop" />
                             </span>
@@ -33,6 +35,27 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-    name: 'FormularioTarefa'
+    name: 'FormularioTarefa',
+    data () {
+        return {
+            timeInSeconds: 0
+        }
+    },
+    computed: {
+        timePassad () : string {
+            return new Date(this.timeInSeconds * 1000).toISOString().substr(11, 8)
+        }
+    },
+    methods: {
+        startTimer () {
+            setInterval(() => { // Contar tempo de duração
+                this.timeInSeconds += 1
+            }, 1000) // 1 second = 1000 milliseconds
+            console.log("Inciando o timer")
+        },
+        stopTimer () {
+            console.log("Parando o timer")
+        }
+    }
 })
 </script>
