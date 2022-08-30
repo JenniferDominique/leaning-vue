ec187f1513645cd532d2a2a3184f5841723b8782
JenniferDominique <j.dominiqueds@gmail.com>
2022-08-29 22:42:57 -0300

    feat(evoluindo_cronometro): Criando o componente Temporizador e criando a lógica de desabilitar os botões do cronometro

--- tracker/src/components/Formulario.vue
+++ tracker/src/components/Formulario.vue
@@ -7,50 +7,20 @@
                 <input type="text" class="input" placeholder="Nome da nova atividade" />
             </div>
             <div class="column">
-                    <div class="is-flex is-align-items-center is-justify-content-space-between">
-                        <Cronometro :timeInSeconds='timeInSeconds'/>
-                        <button class="button" @click="startTimer">
-                            <span class="icon">
-                                <i class="fas fa-play" />
-                            </span>
-                            <span>Play</span>
-                        </button>
-                        <button class="button" @click="stopTimer">
-                            <span class="icon">
-                                <i class="fas fa-stop" />
-                            </span>
-                            <span>Stop</span>
-                        </button>
-                    </div>
-                </div>
+                <Temporizador/>
+            </div>
         </div>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import Cronometro from './Cronometro.vue'
+import Temporizador from './Temporizador.vue'
 
 export default defineComponent({
-    name: 'FormularioTarefa',
+    name: 'Formulario',
     components: {
-        Cronometro
-    },
-    data () {
-        return {
-            timeInSeconds: 0,
-            cronometro: 0
-        }
-    },
-    methods: {
-        startTimer () {
-            this.cronometro = setInterval(() => { // Contar tempo de duração
-                this.timeInSeconds += 1
-            }, 1000) // 1 second = 1000 milliseconds
-        },
-        stopTimer () {
-            clearInterval(this.cronometro)
-        }
+        Temporizador
     }
 })
 </script>
